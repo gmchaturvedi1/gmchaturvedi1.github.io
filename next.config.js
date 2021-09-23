03d8514d77947615cbf0c2567ed59d23993b6bbf
Gajendra Nath Chaturvedi <gajendranathchaturvedi@Gajendras-MacBook-Pro.local>
2021-09-23 11:40:21 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
  env: {
-  PUBLIC_URL: 'https://your-organization-or-username.github.io/my-nextjs-app',
+  PUBLIC_URL: 'https://your-organization-or-username.github.io',
   assetPrefix: './',
  },
  reactStrictMode: true,
